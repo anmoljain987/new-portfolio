c52f94fa222ace6cb64ca06c456cecd85eedda98
Anmol <anmoljain987@gmail.com>
2022-05-14 19:48:15 +0530

    project added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,7 +1,7 @@
 import beauty_image from "../assets/svg/projects/beauty.png";
 import guess_image from "../assets/svg/projects/win.png";
 import adidas_image from "../assets/svg/projects/adidas.png";
-
+import bankman_image from "../assets/svg/projects/Anmol.png";
 export const projectsData = [
   {
     id: 1,
@@ -17,21 +17,31 @@ export const projectsData = [
     id: 2,
     projectName: "Adidas",
     projectDesc:
-      "It is a non functional html css model of website Adidas, we have implemented css with great precision and the website fully responsive.",
+      "It is a non functional html css model of website Adidas just with login functionality, we have implemented css with great precision and the website fully responsive.",
     tags: ["React", "Redux", "CSS", "Bootstrap"],
     code: "https://github.com/anmoljain987/addidas-clone",
     demo: "https://addidas-clone.vercel.app/",
     image: adidas_image,
   },
   {
-    id: 2,
+    id: 3,
     projectName: "Guess-and-Bless",
     projectDesc:
       "It is functional game made using HTML , CSS , JavaScript. This is a simple yet addictive game made just for spending some time",
-    tags: ["HTML", "CSS", "JavaScript"],
+    tags: ["HTML", "CSS", "JavaScript", "Responsive"],
     code: "https://github.com/anmoljain987/guessIt",
     demo: "https://guessandbless.netlify.app/",
     image: guess_image,
+  },
+  {
+    id: 4,
+    projectName: "BankMan",
+    projectDesc:
+      "It is fully functional banking application with login, logout, transfer, conversion.It is very beautifully designed with loaders animation and transitions",
+    tags: ["HTML", "CSS", "JavaScript"],
+    code: "https://github.com/anmoljain987/BankMan",
+    demo: "https://bankman.netlify.app/",
+    image: bankman_image,
   },
 ];
 
